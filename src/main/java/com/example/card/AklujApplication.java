853afbb307a455ef0c101ea4160ec982b12356af
vrushali-138 <vrushalisangar13@gmail.com>
2025-08-06 15:32:05 +0530

    first program

---
+++ src/main/java/com/example/card/AklujApplication.java
@@ -0,0 +1,13 @@
+package com.example.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AklujApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AklujApplication.class, args);
+	}
+
+}
